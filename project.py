9da29ee8077d6c7be61e8d71da75392ac76cdbfb
Kormation <145355933+Kormation@users.noreply.github.com>
2024-01-11 09:33:13 -0500

    n/a
    n/a

--- project.py
+++ project.py
@@ -1,4 +1,4 @@
-# Programmer: 
+# Programmer : Alexander Walker
 # Description: 
 
 # Import and initialize the pygame library
@@ -12,12 +12,21 @@ from ucc_sprite import Sprite
 
 ### SET UP GLOBAL CONSTANTS HERE
 WIDTH = 640
-HEIGHT = 360
-BACKGROUND_COLOR = "white"
+HEIGHT = 640
+BACKGROUND_COLOR = "black"
+
+
+#-=-=-=-=-(Player configure)-=-=-=-=-
+#Controls the starting speed of the player
+plr_speed = 2
+
+plr_x = 100
+plr_y = 100
 
 # Create and open a pygame screen with the given size
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 grid = make_grid()
+grid.show
 
 # Set the title of the pygame screen
 pygame.display.set_caption("My Game")
@@ -29,6 +38,25 @@ clock = pygame.time.Clock()
 all_sprites = pygame.sprite.LayeredUpdates()
 
 ### SET UP YOUR GAME HERE
+
+#-=-=-=-=-=-(Load Images)-=-=-=-=-=-=-
+
+#Player Images.
+player1_image = pygame.image.load("Player1.png")
+player1_image = pygame.transform.rotozoom(player1_image, 0, 0.80)
+player2_image = pygame.image.load("Player2.png")
+player2_image = pygame.transform.rotozoom(player2_image, 0, 0.80)
+
+#-=-=-=-=-=-(Create Sprites)-=-=-=-=-=-
+
+#Player Sprite.
+player = Sprite(player1_image)
+player.center = (plr_x, plr_y)
+player.add(all_sprites)
+player.image_num = 1
+player.count = 0
+
+
 
 
 
@@ -53,13 +81,46 @@ while running:
 
     ### MANAGE GAME STATE FRAME-BY-FRAME
     
-
+    
+    #-=-=-=-=-=-(Player Movement)-=-=-=-=-=-
+    
+    #If the player is alive then.
+    if player.alive():
+        keys = pygame.key.get_pressed()
+        #Moves up if the up arrow is pressed.
+        if keys[K_UP]:
+            #Moves up by plr_speed, which can be configured at the top.
+            player.y -= plr_speed       
+        if keys[K_DOWN]:
+            player.y += plr_speed   
+        if keys[K_LEFT]: 
+            player.x -= plr_speed
+        if keys[K_RIGHT]:
+            player.x += plr_speed
+        
+        #Controls which player sprite is being shown on the screen.
+        if keys[K_DOWN] or keys[K_UP] or keys[K_LEFT] or keys[K_RIGHT]:
+            player.count += 1
+            #Controls how fast the images change.
+            if player.count >= 20:
+                player.count = 0
+                #If player num is == 1 then player2_image will show.
+                if player.image_num == 1:
+                    player.image = player2_image
+                    player.image_num = 2
+                #Else the player1_image will be shown.
+                else:
+                    player.image = player1_image
+                    player.image_num = 1
 
     # Update the sprites' locations
     all_sprites.update()
 
     # Clear the screen
     screen.fill(BACKGROUND_COLOR)
+    
+    #-=-=-=-=-=-=-(Screen Design)-=-=-=-=-=-=-
+    pygame.draw.rect(screen, (255, 255, 255), (200, 50, 10, 330), 0)
 
     # Redraw the sprites
     all_sprites.draw(screen)
@@ -69,3 +130,6 @@ while running:
 
     # Flip the changes to the screen to the computer display
     pygame.display.flip()
+    
+# Quit the pygame program
+pygame.quit()
